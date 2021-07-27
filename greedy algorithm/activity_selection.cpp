cfa8522954e6192e10fb67854b7ae4bf1b54954d
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-07-27 15:28:28 +0530

    Update activity_selection.cpp

--- greedy algorithm/activity_selection.cpp
+++ greedy algorithm/activity_selection.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 struct activity{
 
-    int start, finsish;
+    int start, finish;
 };
 
 bool compare(activity a, activity b){
 
-    return a.finsish < b.finsish; // decreasing order
+    return a.finish < b.finish; 
 }
 
-int getMaxActivities(activity arr[], int n){
+void getMaxActivities(activity arr[], int n){
 
     sort(arr, arr+n, compare);
 
-    cout<<arr[0].start<<" "<<arr[0].finsish<<endl;
+    cout<<arr[0].start<<" "<<arr[0].finish<<endl;
 
     int i=0;
 
     for(int j=1; j<n; j++){
 
-        if(arr[j].start >= arr[i].finsish){
-            cout<<arr[j].start<<" "<<arr[j].finsish<<endl;
+        if(arr[j].start >= arr[i].finish){
+            cout<<arr[j].start<<" "<<arr[j].finish<<endl;
             i=j;
         }
     }
@@ -34,6 +34,12 @@ int main(void){
     int n; cin>>n;
 
     activity arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i].start>>arr[i].finish;
 
-    cout<<getMaxActivities(arr, n);
+    cout<<"---------"<<endl;
+    getMaxActivities(arr, n);
+    
+    for(int i=0; i<n; i++)
+        cout<<arr[i].start<<" "<<arr[i].finish<<endl;
 }
