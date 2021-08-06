6f40f2aa41125fc17feee1e01ba325e5f3bbd3a9
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-08-06 21:46:08 +0530

    Create BFS.cpp

---
+++ graph/BFS.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+vector<int> BFS(vector<int>adj_list[], int n){
+
+    vector<int> bfs;
+    vector<int> visited(n+1, 0); // intitially no node is marked visited
+
+    for(int i=1; i<=n; i++){ // always use for loop as graph can have multiple (disconnected) components
+
+        if(!visited[i]){
+
+            queue<int>q;
+            q.push(i);
+            visited[i]=1; // mark the current node as visited
+
+            while(!q.empty()){
+
+                int node = q.front();
+                bfs.push_back(node); // store the current node in ans
+                q.pop();
+
+                for(auto it: adj_list[i]){ // go to its adjecent nodes
+                    q.push(it);
+                    visited[it]=1;
+                }
+            }
+
+        }
+    }
+}
+
+int main(void){
+}
