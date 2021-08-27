50026c1ebe7403ace87b5037542a6a1f56e4a2f9
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-08-27 13:30:40 +0530

    Create search_in_sorted_and_rotated_array.cpp

---
+++ searching and sorting/search_in_sorted_and_rotated_array.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int searchUtil(vector<int>& nums, int l, int r, int target){
+
+    if(l>r) return -1;
+
+    int m = (l+r)/2;
+
+    if(nums[l] <= nums[m]){ // first half is sorted
+
+        if(target>=nums[l] && target<=nums[m]) 
+            return searchUtil(nums, l, m-1, target);
+
+        return searchUtil(nums, m+1, r, target);
+    }
+
+    else{ // second half is sorted
+
+        if(target>=nums[m] && target<=nums[r])
+            return searchUtil(nums, m+1, r, target);
+
+        return searchUtil(nums, l, m-1, target);
+    }
+
+}
+
+int search(vector<int>& nums, int target){
+
+    return searchUtil(nums, 0, nums.size()-1, target);
+}
+
+int main(void){
+}
