5aa397e5208d4d45042094e7d9deee2e30a75cdb
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-08-29 15:53:42 +0530

    Update count_squares.cpp

--- searching and sorting/count_squares.cpp
+++ searching and sorting/count_squares.cpp
@@ -2,6 +2,7 @@
 #include<math.h>
 using namespace std;
 
+// gievn an integer find no of perfect squares less then it
 int countSquares(int n){
 
     // root(n) generates perfect sqaure then ans is root-1;
