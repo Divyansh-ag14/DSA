20afcddd3321dd92a3a0fd584aefecb020994e42
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-02 13:10:58 +0530

    Update arithematic_number.cpp

--- searching and sorting/arithematic_number.cpp
+++ searching and sorting/arithematic_number.cpp
@@ -13,15 +13,18 @@ int inSequence(int a, int b, int c){
         if((b-a)/c < 0) // this is the formula to get the nth term, so its value cant be negative
             return 0;
 
-        else{
+        if((b-a)%c == 0) return 1;  // if the difference bw numbers is reachable with c return 1
+            
+        return 0;
 
-            if((b-a)%c == 0) return 1;  // if the difference bw numbers is reachable with c return 1
-            return 0;
-
-        }
     }
+
 }
 
 
 int main(void){
+
+    int a=1,b=3,c=2;
+
+    cout<<inSequence(a,b,c);
 }
