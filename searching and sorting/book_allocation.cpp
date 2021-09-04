da80b7fc5083585aeb20dcf4b7f35a6b7bb53ed7
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-04 16:09:30 +0530

    Update book_allocation.cpp

--- searching and sorting/book_allocation.cpp
+++ searching and sorting/book_allocation.cpp
@@ -1,3 +1,19 @@
+/*
+
+You are given N number of books. Every ith book has Ai number of pages. 
+You have to allocate contagious books to M number of students. 
+There can be many ways or permutations to do so. In each permutation, 
+one of the M students will be allocated the maximum number of pages. Out of all these permutations, 
+the task is to find that particular permutation in which the maximum number of pages allocated to a student 
+is minimum of those in all the other permutations and print this minimum value. 
+
+Each book will be allocated to exactly one student. Each student has to be allocated at least one book.
+
+Note: Return -1 if a valid assignment is not possible, and allotment should be in contiguous order 
+
+*/
+
+
 #include<iostream>
 using namespace std;
 
@@ -40,14 +56,14 @@ int findPages(int arr[], int n, int m){
 
         int mid  = (i+j)/2;
 
-        if(isFeasible(arr, n, m, mid)){
+        if(isFeasible(arr, n, m, mid)){ // if current no is valid
 
-            ans = mid;
-            j=mid-1;
+            ans = mid; // update ans
+            j=mid-1; // try to minimize the ans by decreasing the upper bound (j)
 
         }
 
-        else i=mid+1;
+        else i=mid+1; // if ans is not valid: increase lower bound(i)
 
     }
 
