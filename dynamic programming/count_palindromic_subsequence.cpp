5a79453647b8186829851f4e7842f44626c647af
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-11 00:04:51 +0530

    Update count_palindromic_subsequence.cpp

--- dynamic programming/count_palindromic_subsequence.cpp
+++ dynamic programming/count_palindromic_subsequence.cpp
@@ -1,5 +1,12 @@
+/*
+Given a string str of length N, you have to find number of palindromic subsequence 
+(need not necessarily be distinct) which could be formed from the string str.
+Note: You have to return the answer module 109+7;
+*/
+
 #include<iostream>
 #include<string>
+#define mod 1000000007
 using namespace std;
 
 long long int util(int i, int j, string s, long long int **dp){
