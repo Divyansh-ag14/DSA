851744559c9e97260a7f4c59a86691e8fef927b4
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-12 20:06:11 +0530

    Create subset_problem.cpp

---
+++ dynamic programming/subset_problem.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+int util(int arr[], int **dp, int n, int s){
+
+    if(n==-1) return 0; // if no more elements left in the array: return 0
+
+    if(s<0) return 0; // if sum becomes less then 0: return 0
+
+    if(s==0) return 1; // if sum becomes: ans found
+    
+    if(dp[n][s] != -1) return dp[n][s]; // if dp has some value: return it
+
+    // for every element we have 2 choices: take or leave
+    // if we take the element: s becomes s-arr[n]
+    // if we leave the element: s remains same
+    // note n is decreased in both 
+    // any choice can give the ans so use (|| operator)
+    return dp[n][s] = util(arr, dp, n-1, s-arr[n]) || util(arr, dp, n-1, s);
+
+}
+
+int equalPartition(int N, int arr[]){
+
+    int s = 0;
+
+    for(int i=0; i<N; i++)
+        s+=arr[i];
+
+    if(s%2!=0)
+        return 0;
+
+    int **dp = new int*[N];
+
+    for(int i=0; i<N; i++){
+
+        dp[i] = new int[s+1];
+
+        for(int j=0; j<=s; j++)
+            dp[i][j] = -1;
+
+    }
+
+    s = s/2;
+
+    return util(arr, dp, N-1, s);
+
+}
+
+int main(void){
+}
