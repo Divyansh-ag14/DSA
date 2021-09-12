26c2723d3f90e73033e04992d592beac13279008
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-12 19:15:51 +0530

    Update permutation_coefficient.cpp

--- dynamic programming/permutation_coefficient.cpp
+++ dynamic programming/permutation_coefficient.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 
+/* 
+    nPr = n!/(n-r)!
+    n!= n* n-1 * n-2 * ... n-r+1 * (n-r)!
+    => nPr = n * n-1 * n-2 * ... n-r+1
+*/
+
 // T: O(n)  S(n)
 int nPr(int n, int r){
 
@@ -12,6 +18,17 @@ int nPr(int n, int r){
 
     return dp[n]/dp[n-r];
 
+}
+
+// T: O(n)   S: O(1)
+int nPrOptimized(int n, int r){
+
+    int p=1;
+    for(int i=1; i<r; i++)
+        p = p * (n-i);
+
+    return p; 
+    
 }
 
 int main(void){
