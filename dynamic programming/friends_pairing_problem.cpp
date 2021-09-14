f312344bc791daed1c51e81409a351263a34c220
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-14 22:22:56 +0530

    Create friends_pairing_problem.cpp

---
+++ dynamic programming/friends_pairing_problem.cpp
@@ -0,0 +1,38 @@
+/*
+Given N friends, each one can remain single or can be paired up with some other friend. 
+Each friend can be paired only once. Find out the total number of ways in which friends can remain single or can be paired up.
+Note: Since answer can be very large, return your answer mod 10^9+7.
+*/
+
+#include<iostream>
+using namespace std;
+int mod  = 1e9+7;
+
+long long int util(int i, long long int dp[]){
+
+    if(i<=2) return i;
+
+    if(dp[i] != -1) return dp[i];
+
+    // take a person and check its combinations
+    long long int a = (((i-1)%mod) * util(i-2, dp)%mod)%mod;
+    
+    // leave a person
+    long long int b = util(i-1, dp)%mod;
+
+    return dp[i] = a+b;
+
+}
+
+int countFriendsPair(int n){
+
+    long long int dp[n+1];
+    for(int i=0; i<=n; i++)
+        dp[i]=-1;
+
+    return util(0, dp)%mod;
+
+}
+
+int main(void){
+}
