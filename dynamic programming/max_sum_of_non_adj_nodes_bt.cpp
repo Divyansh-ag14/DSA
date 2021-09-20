8db549317f7b3fac6d00caca8b17b24db40050d4
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-21 00:03:25 +0530

    qs desc

--- dynamic programming/max_sum_of_non_adj_nodes_bt.cpp
+++ dynamic programming/max_sum_of_non_adj_nodes_bt.cpp
@@ -1,3 +1,10 @@
+/*
+Given a binary tree with a value associated with each node, 
+we need to choose a subset of these nodes such that sum of chosen nodes is maximum 
+under a constraint that no two chosen node in subset should be directly connected that is, 
+if we have taken a node in our sum then we can’t take its any children or parents in consideration and vice versa.
+*/
+
 #include<iostream>
 #include<unordered_map>
 using namespace std;
