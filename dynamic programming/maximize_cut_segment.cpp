bfa5a1b274e7d4fe94e66fcbace995f2229f07c9
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-24 00:24:40 +0530

    qs desc

--- dynamic programming/maximize_cut_segment.cpp
+++ dynamic programming/maximize_cut_segment.cpp
@@ -1,3 +1,24 @@
+/*
+
+Given an integer N denoting the Length of a line segment. 
+You need to cut the line segment in such a way that the cut length of a line segment each time is either x , y or z. 
+Here x, y, and z are integers.
+After performing all the cut operations, your total number of cut segments must be maximum.
+
+Example:
+
+Input:
+N = 4
+x = 2, y = 1, z = 1
+
+Output: 4
+
+Explanation:Total length is 4, and the cut
+lengths are 2, 1 and 1.  We can make
+maximum 4 segments each of length 1.
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
