14f603fdacdb01ca6c3cd1c8fa641693a025c372
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-09-28 00:35:10 +0530

    Update longest_increasing_subsequence_optimized.cpp

--- dynamic programming/longest increasing subsequence/longest_increasing_subsequence_optimized.cpp
+++ dynamic programming/longest increasing subsequence/longest_increasing_subsequence_optimized.cpp
@@ -1,3 +1,20 @@
+/*
+
+Given an array of integers, find the length of the longest (strictly) increasing subsequence from the given array.
+
+Example:
+
+Input:
+N = 16
+A[]={0,8,4,12,2,10,6,14,1,9,5
+     13,3,11,7,15}
+
+Output: 6
+
+Explanation :Longest increasing subsequence [0 2 6 9 13 15], which has length 6
+
+*/
+
 #include<iostream>
 #include<algorithm>
 #include<limits.h>
