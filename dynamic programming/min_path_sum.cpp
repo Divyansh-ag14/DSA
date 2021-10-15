f9810c7c131645e9f4de11f99c3d926d8ebf7314
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2021-10-15 08:49:18 +0530

    Create min_path_sum.cpp

---
+++ dynamic programming/min_path_sum.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<limits.h>
+using namespace std;
+
+int minPathSum(vector<vector<int>> &grid){
+
+    int rows = grid.size();
+    int cols = grid.size();
+
+    // create a dp array
+    int dp[rows+1][cols+1];
+
+    // last row and and last col will be set to inf
+    for(int i=0; i<=rows; i++)
+        dp[i][cols] = INT_MAX;
+
+    for(int i=0; i<=cols; i++)
+        dp[rows][i] = INT_MAX;
+
+    dp[rows-1][cols]=0; // right to arr
+    dp[rows][cols-1]=0; // down to arr
+
+
+    for(int i=rows-1; i>=0; i--){
+
+        for(int j=cols-1; j>=0; j--){
+
+            dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1]);
+
+        }
+
+    }
+
+    return dp[0][0];
+}
+
+int main(void){
+
+    vector<vector<int>> grid = {{1,3,1},{1,5,1},{4,2,1}};
+    cout<<minPathSum(grid);
+}
