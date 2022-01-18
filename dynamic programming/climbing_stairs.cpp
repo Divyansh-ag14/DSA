e5ae3d5092ba90206f915fbbdb7818bb8944361e
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2022-01-18 17:18:05 +0530

    Create climbing_stairs.cpp

---
+++ dynamic programming/climbing_stairs.cpp
@@ -0,0 +1,38 @@
+/*
+
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int util(int n, int dp[]){
+
+    if(n==0) return 1; // if we have reached the top: add 1 to ans
+    if(dp[n]!=-1) return dp[n]; // if we already have some ans: return it
+
+    // at every step we have 2 options: to take single step or double steps
+    int one=0, two=0;
+    if(n>=1) one = util(n-1, dp); // while taking single step make sure n is atleast 1
+    if(n>=2) two = util(n-2, dp); // while taking double, make sure n is atleast 2
+
+    return dp[n] = one + two; // add the result of both
+
+}
+
+int climbStairs(int n){
+
+    int dp[n+1];
+
+    for(int i=0; i<=n; i++) dp[i]=-1;
+
+    return util1(n, dp);
+
+}
+
+int main(void){
+
+    cout<<climbStairs(2);
+}
