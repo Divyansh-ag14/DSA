47434dcdda2dd7af9e3d39d9da3885e0c11459ce
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2022-01-18 17:19:54 +0530

    new function

--- dynamic programming/climbing_stairs.cpp
+++ dynamic programming/climbing_stairs.cpp
@@ -39,13 +39,23 @@ int util(int n, int dp[]){
 
 }
 
+int util2(int n, int dp[]){
+
+    if(n<0) return 0;
+    if(n==0) return 1;
+
+    if(dp[n] != -1) return dp[n];
+
+    return dp[n] = util2(n-1, dp) + util2(n-2, dp);
+}
+
 int climbStairs(int n){
 
     int dp[n+1];
 
     for(int i=0; i<=n; i++) dp[i]=-1;
 
-    return util1(n, dp);
+    return util(n, dp);
 
 }
 
