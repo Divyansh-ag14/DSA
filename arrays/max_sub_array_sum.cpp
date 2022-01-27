00fd50c90cc4c69066cfa5da325a8bccf77efb96
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2022-01-27 22:26:01 +0530

    Create max_sub_array_sum.cpp

---
+++ arrays/max_sub_array_sum.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+// T: O(n^3)
+int findSum(int arr[], int n){
+
+    int ans=INT8_MIN;
+
+    for(int i=0; i<n; i++){      // sub array is from i to j: that takes 2 loops
+        for(int j=i; j<n; j++){  // use another loop to calculate sum from i to j
+            int s=0;
+            for(int k=i; k<=j; k++){
+                s+=arr[k];
+            }
+            ans=max(ans, s);
+        }
+    }
+}
+
+
+// T: O(n^2)
+int findSum2(int arr[], int n){
+
+    int ans=INT8_MIN;
+
+    for(int i=0; i<n; i++){
+        int s=0;
+        for(int j=i; j<n; j++){
+            s+=arr[j];
+            ans=max(ans, s);
+        }
+    }
+
+}
+
+// T: O(n)
+int findSum3(int arr[], int n){
+
+    int ans=arr[0];
+    int curr_sum=arr[0];
+
+    for(int i=1; i<n; i++){
+
+        curr_sum = max(curr_sum+arr[i], arr[i]);
+        ans = max(ans, curr_sum);
+    }
+}
+
+int main(void){
+}
