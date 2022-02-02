a238eaf7fb05fb398c7cd8dc7de5483454434aa3
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2022-02-02 21:33:36 +0530

    Update pow(x,n).cpp

--- arrays/pow(x,n).cpp
+++ arrays/pow(x,n).cpp
@@ -1,3 +1,16 @@
+/*
+
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+*/
+
 #include<iostream>
 using namespace std;
 
