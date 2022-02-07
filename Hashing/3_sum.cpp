4894d7cd365d335815cf6c3ca04bb619d986e2b5
Divyansh Agarwal <74360856+Divyansh-ag14@users.noreply.github.com>
2022-02-07 23:31:14 +0530

    optimized

--- Hashing/3_sum.cpp
+++ Hashing/3_sum.cpp
@@ -56,7 +56,44 @@ vector<vector<int>> threeSumBrute(vector<int>& nums){
 
 }
 
+// T: O(n^2)
+vector<vector<int>> threeSum(vector<int>& nums) {
+ 
+    int n = nums.size();
+    vector<vector<int>> ans;
+        
+    sort(nums.begin(), nums.end());
+        
+    for(int i=0; i<n-2; i++){
+          
+        if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+            int l=i+1, r=n-1;
+            
+            while(l<r){
+            
+                int res = nums[i] + nums[l] + nums[r];
+        
+                if(res== 0){
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    
+                    while(l<r && nums[l]==nums[l+1]) l++;
+                    while(l<r && nums[r]==nums[r-1]) r--;
+                        
+                    l++; r--;
+                    
+                    }
+                    
+                    else if(res<0) l++;
+                    
+                else r--;
 
+            }
+          
+        }
+            
+    }
+
+}
 
 int main(void){
 }
